36c7036a015d88dfc6adefb18835a92af99d20f0
Douglas Gomes <gomes.douglas07@gmail.com>
2019-08-04 20:36:45 -0300

    Criado classe usuario e arquivo de cofiguração para o mapeamento de entidade pelo EntityFramework

---
+++ Switch.Infra.Data/Context/SwitchContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Switch.Domain.Entities;
+using Switch.Infra.Data.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Infra.Data.Context
+{
+    public class SwitchContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public SwitchContext(DbContextOptions options) : base(options)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //Exemplo Fluent API: 
+
+        /*
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity => {
+                entity.HasKey(u => u.Id);
+
+                entity.Property(u => u.Nome)
+                      .IsRequired()
+                      .HasMaxLength(400);
+
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+        */
+    }
+}
